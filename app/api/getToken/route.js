19c70f74e9fddffa930e7876f127cb675885f352
tanveer-insta-web-works <101490981+tanveer-insta-web-works@users.noreply.github.com>
2023-06-03 13:33:16 +0600

    getToken file changed

--- app/api/getToken/route.js
+++ app/api/getToken/route.js
@@ -4,7 +4,11 @@ import { cookies } from "next/headers";
 import { sign, verify } from "../../lib/utils.js";
 
 export async function GET(request) {
-  const token = request.cookies.get("token");
-  const decodeJwt = await verify(token.value, "zoho");
-  return NextResponse.json({ token: decodeJwt });
+  const token = cookies().get("token");
+  if (token.value) {
+    const decodeJwt = await verify(token.value, "zoho");
+    return NextResponse.json({ token: decodeJwt });
+  } else {
+    return NextResponse.json({ ok: false });
+  }
 }
